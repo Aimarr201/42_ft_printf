ec28e8ae101da67be15b4ad640d938b39249bfc4
Aimar Mendibil Ayo <amendibi@c3r5s1.42urduliz.com>
2026-04-28 19:50:59 +0200

    main

--- main.c
+++ main.c
@@ -4,9 +4,26 @@
 
 int	main(void)
 {
-	char	c = 'C';
-	char	str[] = "string";
-	   printf("Aaaa%saa%ca%%a\n", str, c);
-	ft_printf("Aaaa%saa%ca%%a\n", str, c);
-	return (0);
+	char	*str;
+	char	c;
+	int 	x;
+	void	*ptr;
+
+	c = 'a';
+	str = "Estamos porque hemos venido, que si no, no hubiera pasado nada";
+	x = 123;
+	ptr = &x;
+	ft_printf("El caracter es %c\n", c);
+	printf("El caracter es %c\n", c);
+	ft_printf("String: %s\n", str);
+	printf("String: %s\n", str);
+	ft_printf("Puntero: %p\n", ptr);
+	printf("Puntero: %p\n", ptr);
+	ft_printf("Int max: %d, Int min: %i\n", 0, -521);
+	printf("Int max: %d, Int max: %i\n", 0, 521);
+	ft_printf("Unsigned int: %u\n", 4294967295);
+	printf("Unsigned int: %u\n", 429496729);
+	ft_printf("Hexadecimal en minuscula: %x\n", 255);
+	printf("Hexadecimal en minuscula:: %x\n", 255);
+	ft_printf("Hexadecimal en minuscula: %X\n", 255);
 }
